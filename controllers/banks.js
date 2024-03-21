b30367a3cc0a97e5df672dd5b76906808f9e3036
Andrii Pohuliaiev <andrii.pohuliaiev@gmail.com>
2024-03-21 11:09:48 +0000

    alphabetic order for banks

--- controllers/banks.js
+++ controllers/banks.js
@@ -8,11 +8,14 @@ async function parseBanks() {
   try {
     const banksByCountry = {}
     const data = await parser.parse()
+
     data.forEach(item => {
       const name = item[undefined]
       const countries = item["Countries "]
+
       if (name && countries) {
         const countryCodes = countries.split(" ")
+
         countryCodes.forEach(countryCode => {
           if (!banksByCountry[countryCode]) {
             banksByCountry[countryCode] = new Set()
@@ -34,9 +37,9 @@ async function parseBanks() {
       delete banksByCountry["EEA"]
     }
 
-    // Convert sets to arrays and return
+    // Convert sets to arrays and sort banks alphabetically for each country
     Object.keys(banksByCountry).forEach(countryCode => {
-      banksByCountry[countryCode] = [...banksByCountry[countryCode]]
+      banksByCountry[countryCode] = [...banksByCountry[countryCode]].sort((a, b) => a.localeCompare(b))
     })
 
     return banksByCountry
